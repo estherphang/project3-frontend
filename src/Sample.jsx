fa563a95764378f80f68ba57f4429842a15bf274
estney <esther.pxy@gmail.com>
2024-01-27 15:56:36 +0800

    clear up files. added sample style sheet

---
+++ src/Sample.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Sample() {
+  return (
+    <div>
+      <div className="container">
+        <h1>h1</h1>
+        <h1 className="box">h1</h1>
+        <h2>h2</h2>
+        <h2 className="box"> h2</h2>
+        <h3>h3</h3>
+        <h4 className="box">
+          h4
+          <icon>fake icon</icon>
+        </h4>
+        <p className="contentbox">paragraph for resume/jobs</p>
+        <p>normal paragrah </p>
+        <button className="button"> butoonnnn</button>
+        <p>text field</p>
+      </div>
+    </div>
+  );
+}
