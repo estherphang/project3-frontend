4dc87e21f5950ee18567833baeba7613d7682b03
estney <esther.pxy@gmail.com>
2024-01-30 18:33:35 +0800

    install axios, react-router dom and firebase.

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_SOME_API_KEY,
+  authDomain: import.meta.env.VITE_SOME_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_SOME_DATABASE_URL,
+  projectId: import.meta.env.VITE_SOME_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_SOME_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SOME_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_SOME_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
