f8657397e3ca465d483f34dadc3a31f5349003d9
estney <esther.pxy@gmail.com>
2024-02-03 11:24:28 +0800

    created user-category page

--- src/UserCategory.jsx
+++ src/UserCategory.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //to pass prop to app.jsx for sending to employer/talents link
 
 export default function UserCategory() {
-  return <div>hello, who are you? Talent or Employer?</div>;
+  return (
+    <>
+      <div className="cat-container">
+        <h1 className="cat-title"> Are you an employer or talent?</h1>
+
+        <button className="cat-button">
+          <h4>I am an employer. </h4>
+          <img
+            src="employer.png"
+            style={{
+              width: "65%",
+              height: "auto",
+            }}
+          />
+        </button>
+
+        <br />
+        <br />
+        <button className="cat-button">
+          <h4>I am a talent. </h4>
+          <img
+            src="talent.png"
+            style={{
+              width: "65%",
+              height: "auto",
+            }}
+          />
+        </button>
+        <p className="cat-tnc">
+          Your selection is final and cannot be altered after this stage. Thank
+          you.
+        </p>
+      </div>
+    </>
+  );
 }
